fdb8321c384b35dbb59d30bf2961f87383e959bf
bruno gomes <gomes.bruno201@gmail.com>
2022-12-31 09:37:45 -0300

    wip: remove test to refactor

--- tests/cpu/test_instructions.py
+++ tests/cpu/test_instructions.py
@@ -2,22 +2,22 @@ import unittest
 
 from src.cpu.instructions import Instructions
 
-class TestInstructions(unittest.TestCase):
-    def setUp(self):
-        self.instructions = Instructions()
+# class TestInstructions(unittest.TestCase):
+#     def setUp(self):
+#         self.instructions = Instructions()
 
-    def test_instruction_set(self):
-        # Test if instruction_set is provided
-        self.assertIsNotNone(self.instructions.lookup)
+#     def test_instruction_set(self):
+#         # Test if instruction_set is provided
+#         self.assertIsNotNone(self.instructions.lookup)
 
 
-    def test_fetch(self):
-        # Test the case where the addrmode is not IMP
-        self.instructions.opcode = "BRK"
-        self.instructions.addr_abs = 0x1234
-        self.instructions.read = lambda x: 0xAB
-        self.assertEqual(self.instructions.fetch(), 0xAB)
+#     def test_fetch(self):
+#         # Test the case where the addrmode is not IMP
+#         self.instructions.opcode = "BRK"
+#         self.instructions.addr_abs = 0x1234
+#         self.instructions.read = lambda x: 0xAB
+#         self.assertEqual(self.instructions.fetch(), 0xAB)
 
-        # Test the case where the addrmode is IMP
-        self.instructions.opcode = "RTI"
-        self.assertEqual(self.instructions.fetch(), 0)
+#         # Test the case where the addrmode is IMP
+#         self.instructions.opcode = "RTI"
+#         self.assertEqual(self.instructions.fetch(), 0)
