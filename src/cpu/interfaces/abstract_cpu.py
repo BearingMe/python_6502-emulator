c294341f654eff72358a98aa742233f0dbb721ce
bruno gomes <gomes.bruno201@gmail.com>
2023-01-04 09:53:43 -0300

    fix: updates import

--- src/cpu/interfaces/abstract_cpu.py
+++ src/cpu/interfaces/abstract_cpu.py
@@ -3,14 +3,14 @@ from abc import ABC, abstractmethod
 from .abstract_cpu_addressing_modes import AbstractCpuAddressingModes
 from .abstract_cpu_bus import AbstractCpuBus
 from .abstract_cpu_instructions import AbstractCpuInstructions
-from .abstract_cpu_state_handler import AbstractStateHandler
+from .abstract_cpu_state_handler import AbstractCpuStateHandler
 
 class AbstractCpu(ABC):
     def __init__(self):
         self.addr_modes: AbstractCpuAddressingModes
         self.bus: AbstractCpuBus
         self.instructions: AbstractCpuInstructions
-        self.state: AbstractStateHandler
+        self.state: AbstractCpuStateHandler
 
     @abstractmethod
     def run(self) -> None:
