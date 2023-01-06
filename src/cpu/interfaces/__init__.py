c86f1f1d87bf7217133495cf618127011f02755e
bruno gomes <gomes.bruno201@gmail.com>
2023-01-05 21:56:00 -0300

    refactor: improve interface import
    - change __init__ to reexport abstract class
    
    !files were already updated

---
+++ src/cpu/interfaces/__init__.py
@@ -0,0 +1,5 @@
+from .abstract_cpu import AbstractCpu
+from .abstract_cpu_addressing_modes import AbstractCpuAddressingModes
+from .abstract_cpu_bus import AbstractCpuBus
+from .abstract_cpu_instructions import AbstractCpuInstructions
+from .abstract_cpu_state_handler import AbstractCpuStateHandler
