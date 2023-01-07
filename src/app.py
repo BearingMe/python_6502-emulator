b79009c42463cae447d81171f81605989328ea39
bruno gomes <gomes.bruno201@gmail.com>
2023-01-07 11:42:53 -0300

    refactor: updates import
    - updates cpu exports to be handled by __init__ file
    - updates main file to new import naming pattern

--- src/app.py
+++ src/app.py
@@ -1,8 +1,7 @@
-from cpu.cpu import Cpu
-
+from cpu import Cpu
 
 with open("test.bin", "rb") as f:
     binary = f.read()
 
     cpu = Cpu()
-    cpu.run(binary)
+    cpu.run(binary, cycle_accurate=True)
